ec374cabfe6361fb6b82f7ffbc64e21020ace4ea
Sukhad Adhikari <101186753+sukhad123@users.noreply.github.com>
2025-09-21 17:39:06 -0400

    added first api test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,6 @@ import pluginJs from '@eslint/js';
 
 export default [
   { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-  { languageOptions: { globals: globals.node } },
+  { languageOptions: { globals: { ...globals.node, ...globals.jest } } },
   pluginJs.configs.recommended,
 ];
