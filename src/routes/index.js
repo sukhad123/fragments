a45157feeab07f065a13c2721300772d4a728732
Sukhad Adhikari <101186753+sukhad123@users.noreply.github.com>
2025-11-10 16:14:18 -0500

    test

--- src/routes/index.js
+++ src/routes/index.js
@@ -16,11 +16,11 @@ const router = express.Router();
  * Expose all of our API routes on /v1/* to include an API version.
  */
 // Define our first route, which will be: GET /v1/fragments
-router.use(`/v1/fragments`, authenticate('bearer'), require('./api/get'));
-router.use('/v1/fragments', authenticate('bearer'), require('./api/post'));
+router.use(`/v1/fragments`, authenticate('http'), require('./api/get'));
+router.use('/v1/fragments', authenticate('http'), require('./api/post'));
 //unique id route
 //individual fragment route
-router.use('/v1/fragments', authenticate('bearer'), require('./api/individualFragment.js'));
+router.use('/v1/fragments', authenticate('http'), require('./api/individualFragment.js'));
 // Create a router that we can use to mount our API
 router.get('/', (req, res) => {
   // Client's shouldn't cache this response (always request it fresh)
